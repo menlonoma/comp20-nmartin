34cc4aa3907672f95ebc609f9229a4f5210e24d7
nolanmartin <nolanmartin@sbcglobal.net>
2015-10-12 21:09:01 -0400

    Implemented game.js

---
+++ duckhunt/README.md
@@ -0,0 +1,7 @@
+README.md
+Nolan Martin/10.12.15
+
+Lab 5 Readme
+
+All aspects of the assignment have been completed to my knowledge, with the exception of any animation for extra credit. I discussed the assignment briefly with Sam Gates.
+I have spent approximately 1 hour completing this assignment.
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,14 @@
-// Replace this with your code...
+function init() {
+	
+	var c = document.getElementById('game_canvas');
+	var ctx = c.getContext("2d");
+	var img = new Image();
+	var img2 = new Image();
+	img.addEventListener("load", function() {
+			ctx.drawImage(img, 256, 240);
+			ctx.drawImage(img2, 35, 118, 42, 26, 320, 300, 42, 26);
+			ctx.drawImage(img2, 125, 118, 44, 29, 400, 280, 44, 29);
+	}, false);
+	img.src = "duckhunt-background.gif";
+	img2.src = "duckhunt_various_sheet.png";
+}
