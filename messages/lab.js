5230d37334cb292bfc09af24ae179b3cb4e47edd
nolanmartin <nolanmartin@sbcglobal.net>
2015-10-18 23:43:49 -0400

    modified parse file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here...
+function parse() {
+	var data = new XMLHttpRequest();
+	data.overrideMimeType("application/json");
+	data.open('GET', 'data.json', true);
+
+	var output = "<ul>";
+	for (var i in data.id) {
+		output+="<li>" + data.id[i].content + " " + 
+										data.id[i].username + "</li>";
+	}
+	output+="</ul>";
+	document.getElementById("messages").innerHTML = output;
+}
