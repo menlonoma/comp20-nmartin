ea1cddcf498d0ab2e928fc62c478f027ce470911
nolanmartin <nolanmartin@sbcglobal.net>
2015-10-28 23:12:42 -0400

    minor css formatting changes

--- mapchat/main.js
+++ mapchat/main.js
@@ -15,21 +15,21 @@ function init() {
 			navigator.geolocation.getCurrentPosition(sendLoc);
 
 		}
-		/*else {
-			x.innerHTML = "Geolocation is not supported by this browser.";
-		}*/
 }
 
 function sendLoc(position) {
 		// Move to new location
-		myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		myLocation = new google.maps.LatLng(position.coords.latitude, 
+			position.coords.longitude);
 		map.panTo(myLocation);
 		var login = "CalvinGraham";
-		var message = "Hello"
+		var message = "Hello";
+
 		var xhr = new XMLHttpRequest();
 		xhr.open("POST", "https://secret-about-box.herokuapp.com/sendLocation", true);
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.send("login=" + login + "&lat=" + position.coords.latitude + "&lng=" + position.coords.longitude + "&message=" + message);
+		xhr.send("login=" + login + "&lat=" + position.coords.latitude + "&lng=" + 
+			position.coords.longitude + "&message=" + message);
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4 && xhr.status == 200) {
 				var data = JSON.parse(xhr.responseText);
@@ -52,23 +52,27 @@ function create (data, iter) {
 		var lng = data[iter].lng;
 		var lat = data[iter].lat;
 		var landmark = new google.maps.LatLng(lat, lng);
+		//if this is users data
 		if (data[iter].login == "CalvinGraham") {
 			icon = new google.maps.Marker({
 				position: landmark, map: map, title: data[iter].login,
 				icon: "arrow.png"
 			});
 			icon.setMap(map);
-			contentString = data[iter].login;
+			contentString = "<div id=login>" + data[iter].login;
 	  		var infowindow = new google.maps.InfoWindow({
 	  			content: contentString
 	  		});
   		}
+  		//all other data
   		else {
 			icon = new google.maps.Marker({
 				position: landmark, map: map, title: data[iter].login
 			});
 			icon.setMap(map);
-			contentString = data[iter].login + "<p>" + data[iter].message + "</p>" + "<p>" + "Distance: " + distance(lat, lng) + " miles away" + "</p>";
+			contentString = "<div id=login>" + data[iter].login + "</div>" + 
+			"<p>" + data[iter].message + "</p>" + "<p>" + "Distance: " + 
+			distance(lat, lng) + " miles away" + "</p>";
 	  		var infowindow = new google.maps.InfoWindow({
 	  			content: contentString
 	  		});
@@ -85,7 +89,7 @@ function create (data, iter) {
 function distance (lat, lng) {
 	mylat = myLocation.lat();
 	mylng = myLocation.lng();
-	var R = 6371
+	var R = 6371 //kilometers
 	var latdiff = mylat - lat;
 	var radlat = toRad(latdiff);
 	var lngdiff = mylng - lng;
@@ -94,7 +98,7 @@ function distance (lat, lng) {
 		* Math.cos(toRad(mylat)) * Math.sin(radlng/2) * Math.sin(radlng/2);
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 	var d = R * c;
-	return d/1.609344;
+	return d/1.609344; //convert to miles
 
 }
 
